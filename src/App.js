08f730ff66c36025ca063482aa291bc17c80a4a1
selvamurugaiah <selvamurugaiah100@gmail.com>
2023-04-21 08:45:34 +0530

    auth-fe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+export const url ="http://localhost:8000" //"https://auth-be-brb9.onrender.com"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <BrowserRouter>
+  <Routes>
+   <Route path='/login' element={<Login/>}/>
+   <Route path='/dashboard' element={<Dashboard/>}/>
+   <Route path='*' element={<Navigate to ='/login'/>}/>
+  </Routes>
+  </BrowserRouter>
+  
+  </>
 }
 
 export default App;
